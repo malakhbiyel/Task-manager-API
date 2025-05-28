f5e506ba69dd024c39488918d50275ef13aa8cbc
malakhbiyel <malak_hbiyel@outlook.fr>
2025-05-28 17:24:11 +0200

    added the mappers

--- src/main/java/devxplorer/task_manager_api/mapper/TaskMapper.java
+++ src/main/java/devxplorer/task_manager_api/mapper/TaskMapper.java
@@ -1,2 +1,36 @@
-package devxplorer.task_manager_api.mapper;public class TaskMapper {
+package devxplorer.task_manager_api.mapper;
+
+
+import devxplorer.task_manager_api.dto.TaskCreateDTO;
+import devxplorer.task_manager_api.dto.TaskDTO;
+import devxplorer.task_manager_api.model.Task;
+import devxplorer.task_manager_api.model.User;
+
+public class TaskMapper {
+
+    public static TaskDTO toDTO(Task task) {
+        if (task == null) return null;
+
+        return new TaskDTO(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getStatus(),
+                task.getDueDate(),
+                task.getUser() != null ? task.getUser().getId() : null
+        );
+    }
+
+    public static Task fromCreateDTO(TaskCreateDTO dto, User user) {
+        if (dto == null) return null;
+
+        Task task = new Task();
+        task.setTitle(dto.getTitle());
+        task.setDescription(dto.getDescription());
+        task.setDueDate(dto.getDueDate());
+        task.setUser(user);
+
+        return task;
+    }
 }
+
--- src/main/java/devxplorer/task_manager_api/mapper/UserMapper.java
+++ src/main/java/devxplorer/task_manager_api/mapper/UserMapper.java
@@ -1,2 +1,32 @@
-package devxplorer.task_manager_api.mapper;public class UserMapper {
+package devxplorer.task_manager_api.mapper;
+
+
+import devxplorer.task_manager_api.dto.UserCreateDTO;
+import devxplorer.task_manager_api.dto.UserDTO;
+
+import devxplorer.task_manager_api.model.User;
+
+public class UserMapper {
+
+    public static UserDTO toDTO(User user) {
+        if (user == null) return null;
+
+        return new UserDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail()
+        );
+    }
+
+    public static User fromCreateDTO(UserCreateDTO dto) {
+        if (dto == null) return null;
+
+        User user = new User();
+        user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+
+        return user;
+    }
 }
+
