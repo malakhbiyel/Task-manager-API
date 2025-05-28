6f432a56f83ac4de06832768c0290c4e66ae2f6c
malakhbiyel <malak_hbiyel@outlook.fr>
2025-05-28 17:27:42 +0200

    added the repositories

---
+++ src/main/java/devxplorer/task_manager_api/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package devxplorer.task_manager_api.repository;
+
+import devxplorer.task_manager_api.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(Long userId);
+}
---
+++ src/main/java/devxplorer/task_manager_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package devxplorer.task_manager_api.repository;
+
+import devxplorer.task_manager_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
